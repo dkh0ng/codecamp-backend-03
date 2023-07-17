cdee2caa7abc4da8a4c248b8cbb5f69596f93bd2
dkh0ng <dkh1721@gmail.com>
2023-07-17 10:20:02 +0900

    first commit

---
+++ class/01-01-token/index.js
@@ -0,0 +1,11 @@
+console.log("안녕하세요");
+
+function getToken(aaa) {
+  const result = String(Math.floor(Math.random() * 10 ** aaa)).padStart(
+    aaa,
+    "0"
+  );
+  console.log(result);
+}
+
+getToken(8);
