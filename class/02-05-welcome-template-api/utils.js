1fa8d8e77533fba4421337e0cd6b32373224c01f
dkh0ng <138542255+dkh0ng@users.noreply.github.com>
2023-12-13 15:24:28 +0900

    Delete class directory

--- class/02-05-welcome-template-api/utils.js
+++
@@ -1,8 +0,0 @@
-export function getToday() {
-  const aaa = new Date();
-  const year = aaa.getFullYear();
-  const month = aaa.getMonth() + 1;
-  const date = aaa.getDate();
-  const today = `${year}-${month}-${date}`;
-  return today;
-}
